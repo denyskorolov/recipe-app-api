e617a110a24c343e9c5e4b157d13f52c572242d9
Denys Korolov <denys.korolov@canstudios.com>
2024-02-15 22:16:21 +0000

    implemented custom user model

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,8 +11,9 @@ from django.test import SimpleTestCase
 
 
 @patch("core.management.commands.wait_for_db.Command.check")
-class CommandTests(SimpleTestCase):
+class CommandsTests(SimpleTestCase):
     """Test commands"""
+
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready"""
